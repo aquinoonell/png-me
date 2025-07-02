d3694c9483f6fd5596545997a0af1bda506fff9e
don <onellaquino@icloud.com>
2025-07-01 22:21:15 -0400

    Impl Commands

--- src/commands.rs
+++ src/commands.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, str};
 use std::fs::File;
 use std::io::Read;
 use std::str::FromStr;
@@ -9,4 +9,17 @@ use crate::png::Png;
 
 pub fn print (path: &str) {
     let buffer = get_bytes_from_path(path);
+}
+
+
+pub fn remove(path: &str, chunk_type: &str) -> std::io::Result<bool>{
+    let buffer = get_bytes_from_path(path);
+    let mut png = Png::try_from(buffer.as_slice()).unwrap();
+}
+
+fn get_bytes_from_path(path: &str) -> Vec<u8>{
+    let mut f = File::open(path).expect("No file found");
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer).expect("Buffer overflow");
+    buffer
 }
